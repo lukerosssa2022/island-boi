e120e0dabb4c098a6d949852360bbddf5a8dee09
luk ross <lukerosssa@gmail.com>
2022-02-21 16:55:51 +0200

    added booking form and the breate method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.island = Island.find(params[:island_id])
+    if @booking.save
+      redirect_to bookings_path, notice: "Great your booking was created"
+    else
+      render "islands/show"
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :state)
+  end
 end
