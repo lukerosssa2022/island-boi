3fa71766ce02c29b39537d3b5632f9254fbf8d38
ofek shtrit <ofek1997shtrit@gmail.com>
2022-02-21 12:37:31 +0200

    added a delete btn on the view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :islands, only: %i[index new create]
+  resources :islands, only: %i[index new create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
