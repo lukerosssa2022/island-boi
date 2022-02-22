229434af24bbb2fbc3e4b7e2f34b502330ea6bc6
luk ross <lukerosssa@gmail.com>
2022-02-22 12:46:42 +0200

    island new page fixed

--- app/models/island.rb
+++ app/models/island.rb
@@ -2,5 +2,7 @@ class Island < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  COUNTRIES = ["Isreal", "Spain", "Germany", "South Africa"]
+
   validates :name, :country, :price_per_day, :size, presence: true
 end
