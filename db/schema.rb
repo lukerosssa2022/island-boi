d7dbe7fce01c46a29e76079a2b7112fb8ff13aad
Daniel tukevkin <ekvdnl@gmail.com>
2022-02-23 15:22:54 +0200

    schema minor update

--- db/schema.rb
+++ db/schema.rb
@@ -43,15 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_23_124048) do
     t.index ["user_id"], name: "index_islands_on_user_id"
   end
 
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
